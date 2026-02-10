1bff43ec48c1b828e3809b05a8a7791aba5775e4
Krishan <chiranjiv.shah783@gmail.com>
2026-02-10 11:59:18 +0530

    Updated health.js, imported email

--- routes/health.js
+++ routes/health.js
@@ -1,21 +1,22 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const OFFICIAL_EMAIL = process.env.EMAIL;
 
-router.get('/', async(req, res) => {
-        try {
+try {
+        router.get('/', (req, res) => {
                 res.status(200).json({
                         is_success: true,
-                        official_email: OFFICIAL_EMAIL,
+                        official_email: OFFICIAL_EMAIL
                 });
-        } catch (err) {
-                console.error("API ERROR:", error);
+        });
+} catch (err) {
+        console.error("API ERROR:", err);
 
-                res.status(400).json({
-                        is_success: false,
-                        official_email: OFFICIAL_EMAIL,
-                        error: error.message || "Unknown server error"
-                });
-        }
-})
+        res.status(400).json({
+                is_success: false,
+                official_email: OFFICIAL_EMAIL,
+                error: err.message || "Unknown server error"
+        });
+}
 
 module.exports = router;
