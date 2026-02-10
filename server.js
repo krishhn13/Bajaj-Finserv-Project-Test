d9dcc334101ad1886c8ed18dfb988688df82d3d1
Krishan <chiranjiv.shah783@gmail.com>
2026-02-10 11:59:51 +0530

    Made /health api working by calling it in server

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const express = require('express')
-const bfhl = require('./routes/bfhl')
 const app = express()
 const PORT = process.env.PORT
+const bfhl = require('./routes/bfhl')
+const health = require('./routes/health')
 
 app.use(express.json())
 app.use('/bfhl', bfhl)
+app.use('/health', health)
 
 
 app.listen(PORT, (req, res)=>{
